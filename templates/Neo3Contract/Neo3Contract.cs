28c34ab4882c29a438336dfe4ccc2978634dab3e
Shuai <bfshm@qq.com>
2021-03-19 11:02:05 +0800

    init

---
+++ templates/Neo3Contract/Neo3Contract.cs
@@ -0,0 +1,19 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.ComponentModel;
+
+namespace Neo3Contract
+{
+    [DisplayName("Neo3Contract")]
+    [ManifestExtra("Author", "NEO")]
+    [ManifestExtra("Email", "developer@neo.org")]
+    [ManifestExtra("Description", "This is a Neo3Contract")]
+    class Neo3Contract : SmartContract
+    {
+        public static bool Main()
+        {
+            return true;
+        }
+    }
+}
